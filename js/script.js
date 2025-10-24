73b27a14d59221ebb5d9e31d6ad7c1e2de4ea564
Stefani Viranessa <stefaniviranessa@gmail.com>
2025-10-24 09:31:51 +0700

    Add Project File

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// ANIMASI BAGIAN LANDING PAGE HERO
+document.addEventListener('DOMContentLoaded', function() {
+    // Konfigurasi Typed.js
+    const options = {
+        strings: ['predictive models.', 'interactive dashboards.', 'solutions with data.'],
+        typeSpeed: 50,
+        backSpeed: 30,
+        backDelay: 2000,
+        loop: true,
+        showCursor: true,
+        cursorChar: '|',
+    };
+
+    // Inisialisasi Typed.js
+    const typed = new Typed('#typed-text', options);
+});
+
+// ANIMASI TECH STACK
+const techItems = document.querySelectorAll('.tech-item');
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          
+          entry.target.classList.add('visible', 'fade-in'); // variasi animasi 
+          observer.unobserve(entry.target); // hanya animasi sekali
+        }
+    });
+}, { threshold: 0.3 });
+
+// delay bertahap
+techItems.forEach(item => observer.observe(item));
+
+techItems.forEach((item, index) => {
+    item.style.transitionDelay = `${index * 0.1}s`;
+    observer.observe(item);
+});
